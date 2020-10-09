2e7fbf45c4fbd3da5729bfaedaddf0fe9dafb651
minh tien <tienbm90@gmail.com>
2020-10-09 16:02:35 +0700

    add policy api

--- auth/enforcer.go
+++ auth/enforcer.go
@@ -1,23 +1,29 @@
 package auth
 
 import (
+	"fmt"
 	"github.com/casbin/casbin/v2"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
 	"log"
 )
 
-
 func NewCasbinEnforcer(connStr string) *casbin.Enforcer {
 	adapter, err := gormadapter.NewAdapter("mysql", connStr)
 	if err != nil {
-		log.Fatal("Sqlerror: %s", err.Error())
+		log.Fatal(fmt.Sprintf("Sqlerror: %s", err.Error()))
 	}
 	enforcer, err := casbin.NewEnforcer("config/rbac_model.conf", adapter)
 	if err != nil {
-		log.Fatal("Creating enforcer error: %s", err.Error())
+		log.Fatal(fmt.Sprintf("Creating enforcer error: %s", err.Error()))
 	}
 
-	enforcer.AddPolicy()
-
+	////create default policy
+	//enforcer.AddPolicy("admin","/auth/policy", "GET")
+	//enforcer.AddPolicy("admin","/auth/policy", "POST")
+	////create default policy
+	//enforcer.AddPolicy("admin","/auth/grouppolicy/*", "GET")
+	//enforcer.AddPolicy("admin","/auth/grouppolicy", "POST")
+	//
+	//enforcer.LoadPolicy()
 	return enforcer
 }
